2fd8ee72a56301411c133214d52360d474c813f9
Theo <theosef@yahoo.com>
2022-04-15 19:28:31 +0100

    Switching output to XML

--- nmapscan.py
+++ nmapscan.py
@@ -3,66 +3,28 @@ import os
 
 Scan = nmap.PortScanner()
 
-if os.path.exists("oldOutput.txt"):
-    os.remove("oldOutput.txt")
+if os.path.exists("oldOutput.xml"):
+    os.remove("oldOutput.xml")
 
-if os.path.exists("newOutput.txt"):
-    os.rename("newOutput.txt", "oldOutput.txt")
+if os.path.exists("newOutput.xml"):
+    os.rename("newOutput.xml", "oldOutput.xml")
 
 hostFile = open('targets.txt')
 ipFile = open("IP list.txt", "w")
-output = open("newOutput.txt", "w")
+xmlOutput = open("newOutput.xml", "wb")
 targets = hostFile.read()
 hostFile.close()
 
-first = True
 
 if len(targets) != 0:
     Scan.scan(hosts=targets, arguments='-T4')
     print(targets)
     print('Scan Complete!')
-    for host in Scan.all_hosts():
-        if first:
-            first = False
-        else:
-            ipFile.write(", ")
-
-        ipFile.write(host)
-
-        print('----------------------------------------------------')
-        print('Host : %s (%s)' % (host, Scan[host].hostname()))
-
-        output.write(host + ',')
-
-        mac = "-"
-        vendorName = "-"
-
-        if 'mac' in Scan[host]['addresses']:
-            mac = Scan[host]['addresses']['mac']
-            output.write(mac)
-
-        output.write(',')
-
-        print('State : %s' % Scan[host].state())
-        if Scan[host].all_protocols():
-            for proto in Scan[host].all_protocols():
-                print('----------')
-                print('Protocol : %s' % proto)
-                #output.write(proto + "; ")
-                ports = Scan[host][proto].keys()
-
-                fst = True
-                for port in ports:
-                    if fst:
-                        fst = False
-                    else:
-                        output.write(",")
-                    output.write(proto.upper() + str(port) + ":" + Scan[host][proto][port]['state'])
-                    print('port : %s\t state : %s' % (port, Scan[host][proto][port]['state']))
-                output.write("\n")
-        else:
-            output.write("\n")
+    xmlOutput.write(Scan.get_nmap_last_output())
+    print("XML Output:\n")
+    print(str(Scan.get_nmap_last_output()))
 else:
     print('Targets list is empty, exiting.')
 
 ipFile.close()
+xmlOutput.close()
