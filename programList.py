289ac4a737c9440fbebeb962b57f1f4df466b137
GrimoireZero <evanwinters12345@gmail.com>
2022-04-18 20:53:11 +0100

    Git >_<

--- programList.py
+++ programList.py
@@ -1,21 +1,29 @@
-import subprocess
-import time
 from datetime import datetime
 import os
 import glob
-import csv
 import os.path
 from paramiko import SSHClient, AutoAddPolicy
 from rich import print
 
 save_path = 'C:/Results'  # Directory for all results
 absolute_file = str("")  # Unique File name bundled with Directory (C:/Results/"PCname"/)
+result_file = ""
+sub_directory = ""
+machine_name = ""
+master_file = "C:/Results/MasterFile.csv"
+
+
+def create_master():  # Creates master file if not already created
+    if not os.path.exists(master_file):
+        f = open(master_file, 'x')
+        f.close()
+    open(master_file, 'w').close()  # Wipes master file on each run
 
 
 def create_directories(hostname):  # Create Results Directory, and subdirectories, if they do not already exist
-    global absolute_file
+    global absolute_file, sub_directory
     directory = "C:/Results/"
-    sub_directory = directory + hostname
+    sub_directory = directory + hostname + "Logs"
     print(sub_directory)
     if not os.path.exists(directory):  # Create Results Directory if it does not already exist
         os.makedirs(directory)
@@ -24,10 +32,11 @@ def create_directories(hostname):  # Create Results Directory, and subdirectorie
 
     absolute_file = sub_directory
 
-    return absolute_file
+    return absolute_file, sub_directory
 
 
 def remote_connection():  # Code to connect to other machines, run batch file and write output to file
+    global machine_name
     hosts = ['192.168.0.32', '192.168.0.33']  # List of IPs for client PCs
 
     for host in hosts:
@@ -41,7 +50,6 @@ def remote_connection():  # Code to connect to other machines, run batch file an
         # available
         client.connect(host, username='SecurityAdmin')
 
-        # commands = ['echo Test', 'hostname']
         host_command = 'hostname'
         commands = ['softwareList.bat']  # Name of program on Remote PC
 
@@ -52,6 +60,7 @@ def remote_connection():  # Code to connect to other machines, run batch file an
             hostname = f'{stdout.read().decode("utf8")}'
             print(hostname)  # DEBUG CODE REMOVE WHEN FINISHED
             hostname = hostname.strip('\r\n')
+            machine_name = hostname
             create_directories(hostname)  # Pass PC name to create_directories function
 
         else:
@@ -60,7 +69,7 @@ def remote_connection():  # Code to connect to other machines, run batch file an
         stdout.close()
         stderr.close()
 
-        generate_file()  # Run generate_file function, has to be done after successfully getting hostname
+        generate_file(hostname)  # Run generate_file function, has to be done after successfully getting hostname
 
         global absolute_file
         f = open(absolute_file, "w")
@@ -74,6 +83,8 @@ def remote_connection():  # Code to connect to other machines, run batch file an
                 print(output)
                 for line in output:
                     f.write(line)  # Writing output to csv file
+                f.close()
+                compare_programs()
             else:
                 print(f'STDERR: {stderr.read().decode("utf8")}')  # Displays error message
 
@@ -82,89 +93,79 @@ def remote_connection():  # Code to connect to other machines, run batch file an
         client.close()
 
 
-"""
-# Old code that only ran on host machine, will remove in final version
-def find_programs():
-    global absolute_file
-    f = open(absolute_file, "wb")
-
-    output = subprocess.Popen(
-        "softwareList.bat",
-        stdout=subprocess.PIPE).stdout
-
-    for line in output:
-        f.write(line)  # Writing output to file
-
-    output.close()
-    print("Finished Successfully!")
-    print(absolute_file)  # TEST LINE REMOVE WHEN FINISHED
-    return
-"""
-
-
 def diff(lines1, lines2):  # Function to compare lists
     return list(set(lines1) - set(lines2)) + list(set(lines2) - set(lines1))  #
 
 
 def compare_programs():  # Does not work with current version
-    global absolute_file
+    global absolute_file, result_file, sub_directory, machine_name, master_file
 
-    list_of_files = glob.glob('C:/Results/*.csv')  # Retrieves list of files in directory using the .csv format
-    sorted_files = sorted(list_of_files, key=os.path.getctime)
+    list_of_files = glob.glob(sub_directory + '\*.csv')  # Retrieves list of files in directory using the .csv format
+    list_of_files.sort(reverse=True, key=os.path.getctime)
+    print("This is the list of files", list_of_files)
 
-    number_of_files = len(os.listdir("C:/Results"))
+    number_of_files = len(os.listdir(sub_directory))
     if number_of_files > 1:
 
-        print("COMPARE TEST CHECKPOINT 1")  # DEBUG CODE
+        t1 = open(list_of_files[0], 'r')
+        file1 = t1.readlines()
+        t1.close()
 
-        f1 = open(absolute_file, "r")  # Opens the newest file
-        f2 = open(sorted_files[-2], "r")  # Opens the second-newest file
+        t2 = open(list_of_files[1], 'r')
+        file2 = t2.readlines()
+        t2.close()
 
-        lines1 = f1.readlines()  # Stored first file in list
-        lines2 = f2.readlines()  # Stores second file in list
+        print("This is file1 :", file1)
+        print("This is file2 :", file2)
 
-        diff_results = (diff(lines1, lines2))  # Use function to find differences between the two lists
+        with open(result_file, 'w') as f:
+            f.write(machine_name + '\n')
+            for line in file1:
+                if line not in file2:
+                    f.write(line)
+        f.close()
 
-        # Code to tidy up the output
-        diff_results = list(map(lambda x: x.replace('DisplayName : ', '').replace('\n', ''), diff_results))
+        with open(result_file, 'r') as firstfile, open(master_file, 'a') as secondfile:
+            for line in firstfile:  # Writing each line from result file to master file
+                secondfile.write(line + '\n')
+            secondfile.write('\n')
 
-        diff_results2 = str(diff_results)[1:-1]  # Removes Square Brackets from output
-        print(diff_results2)
-        data = [diff_results2]
-
-        with open("newPrograms.csv", 'w') as f:
-
-            writer = csv.writer(f)
-
-            writer.writerow(data)  # Write output to CSV file
+        firstfile.close()
+        secondfile.close()
 
     elif number_of_files < 1:  # Code to catch exception. Not sure how this would occur but coded in just for safety
         print("ERROR: There should be at least 1 file present in the directory")
-        quit()
+        return
 
     else:  # On first use of the program, only one file is generated. Halts program before comparison fails
         print("Initial setup Complete. Run the program again to generate another file and compare installed programs.")
-        quit()
+        return
 
 
-def generate_file():
+def generate_file(hostname):
     current_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")  # Using Date and time for unique file name
-    global absolute_file
-    file1 = current_datetime + ".csv"
+    global absolute_file, result_file, master_file
+    directory = "C:/Results/"
+    filename = current_datetime + ".csv"
+    result_file = hostname + ".csv"
     global save_path
-    absolute_file = os.path.join(absolute_file, file1)  # Combing directory path and unique file name
-    print(absolute_file)  # TESTING CODE
+    absolute_file = os.path.join(absolute_file, filename)  # Combing directory path and unique file name
+    result_file = directory + result_file
     f = open(absolute_file, 'x')  # Creating unique file in directory
 
     print("New File created : ", absolute_file)
     f.close()
 
-    return absolute_file
+    if not os.path.exists(result_file):  # Check to see if file already exists, if not, creates it
+        f2 = open(result_file, 'x')  # Creating file for comparison results (1 per client machine)
+        f2.close()
+
+    return absolute_file, result_file
 
 
 def run_script():
+    create_master()
     remote_connection()
-    # compare_programs()  # Not fully implemented for current version
 
 
 run_script()
