b820fb0cc2f7ca8d29b882da8d1f275557c11573
Theo <theosef@yahoo.com>
2022-04-20 18:40:02 +0100

    Adding back the ip list output

--- nmapscan.py
+++ nmapscan.py
@@ -14,15 +14,24 @@ ipFile = open("IP list.txt", "w")
 xmlOutput = open("newOutput.xml", "wb")
 targets = hostFile.read()
 hostFile.close()
-
+first = True
 
 if len(targets) != 0:
     Scan.scan(hosts=targets, arguments='-T4')
     print(targets)
     print('Scan Complete!')
+
     xmlOutput.write(Scan.get_nmap_last_output())
     print("XML Output:\n")
     print(str(Scan.get_nmap_last_output()))
+
+    for host in Scan.all_hosts():
+        if first:
+            first = False
+        else:
+            ipFile.write(", ")
+
+        ipFile.write(host)
 else:
     print('Targets list is empty, exiting.')
 
